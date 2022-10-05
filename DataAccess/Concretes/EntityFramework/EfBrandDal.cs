090bbc469706003965c6a67b5add212ddf0608e1
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-10-05 12:34:02 +0300

    Add crud operations with entity framework

--- DataAccess/Concretes/EntityFramework/EfBrandDal.cs
+++ DataAccess/Concretes/EntityFramework/EfBrandDal.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DataAccess.Abstracts;
 using DataAccess.Concretes.EntityFramework.Contexts;
 using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concretes.EntityFramework
 {
@@ -13,17 +14,26 @@ namespace DataAccess.Concretes.EntityFramework
     {
         public Brand? GetById(int id)
         {
-            throw new NotImplementedException();
+            using (RentACarBoschContext context = new())
+            {
+                return context.Brands.SingleOrDefault(b => b.Id == id);
+            }
         }
 
         public Brand? GetByName(string name)
         {
-            throw new NotImplementedException();
+            using (RentACarBoschContext context = new())
+            {
+                return context.Brands.FirstOrDefault(b => b.Name == name);
+            }
         }
 
         public List<Brand> GetList()
         {
-            throw new NotImplementedException();
+            using (RentACarBoschContext context = new())
+            {
+                return context.Brands.ToList(); // select * from brands
+            }
         }
 
         public void Add(Brand brand)
@@ -39,12 +49,22 @@ namespace DataAccess.Concretes.EntityFramework
 
         public void Update(Brand brand)
         {
-            throw new NotImplementedException();
+            using (RentACarBoschContext context = new())
+            {
+                var entity = context.Entry(brand); // Parametre olarak gelen nesne'nin referansını yakala
+                entity.State = EntityState.Modified; // Entry'ın durumunu güncelliyoruz.
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Brand brand)
         {
-            throw new NotImplementedException();
+            using (RentACarBoschContext context = new())
+            {
+                var entity = context.Entry(brand);
+                entity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
     }
 }
