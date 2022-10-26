1d58910c931196a109f153ffb04421c9a1ee3b32
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-10-26 15:40:53 +0300

    Begin adding logging manager

---
+++ Core/CrossCuttingConcerns/Logging/ILoggingManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+
+public interface ILoggingManager
+{
+    public void Error(string message);
+    public void Fatal(string message);
+    public void Info(string message);
+    public void Warn(string message);
+    public void Debug(string message);
+    public void Verbose(string message);
+}
---
+++ Core/CrossCuttingConcerns/Logging/Serilog/LoggerServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog;
+
+public abstract class LoggerServiceBase : ILoggingManager
+{
+    protected ILogger _logger { get; set; }
+
+    public virtual void Error(string message) => _logger.Error(message);
+    public virtual void Fatal(string message) => _logger.Fatal(message);
+    public virtual void Info(string message) => _logger.Information(message);
+    public virtual void Warn(string message) => _logger.Warning(message);
+    public virtual void Debug(string message) => _logger.Debug(message);
+    public virtual void Verbose(string message) => _logger.Verbose(message);
+}
